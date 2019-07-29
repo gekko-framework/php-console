82748219ac814eae559863188c1e1f539b67d3ad
Leo Brugnara <leobr6@gmail.com>
2019-07-29 16:45:37 -0300

    Add an abstract PHPCommand class with shared functionality

--- src/Console/PHP/FastCGICommand.php
+++ src/Console/PHP/FastCGICommand.php
@@ -4,8 +4,13 @@ namespace Gekko\Console\PHP;
 
 use \Gekko\Console\{ ConsoleContext, Command};
 
-class FastCGICommand extends Command
+class FastCGICommand extends PHPCommand
 {
+    public function __construct(ConsoleContext $ctx)
+    {
+        parent::__construct($ctx);
+    }
+
     public function run(ConsoleContext $ctx) : int
     {
         // argv[1] should be the FastCGICommand's name
@@ -17,104 +22,27 @@ class FastCGICommand extends Command
 
         $operation = $ctx->getArguments()[2];
         
-        $is_windows = strcasecmp(substr(PHP_OS, 0, 3), 'WIN') == 0;
-
         if ($operation === "start")
         {
-            if ($is_windows) {
-                $this->startWindows($ctx);
-            } 
+            $pid = $this->start($ctx);
 
-            return 0;
+            return $pid > 0 ? 0 : -1;
         }
         else if ($operation === "stop")
         {
-            if ($is_windows) {
-                $this->stopWindows($ctx);
-            }
+            return $this->stop($ctx) ? 0 : -1;
         }
 
         return -2;
     }
 
-    private function startWindows(ConsoleContext $ctx) : int
+    public function start(ConsoleContext $ctx) : int
     {
-        $dir = $ctx->toLocalPath("/.tmp/php");
-
-        if (!\file_exists($dir))
-            mkdir($dir);
-
-        $pid_file = "{$dir}/.php-cgi.pid";
-
-        if (\file_exists($pid_file))
-        {
-            \fwrite(STDOUT, "PHP server is already running (PID " . \file_get_contents($pid_file) .  ")");
-            return -1;
-        }
-
-        $previous_pids = [];
-        $fd = popen("tasklist /fi \"imagename eq php-cgi.exe\" /fo CSV /nh", "r");
-        $output = stream_get_contents($fd);
-        pclose($fd);
-
-        $lines = explode("\n", $output);
-
-        foreach ($lines as $line)
-        {
-            $columns = explode("\",\"", $line);
-            
-            if (isset($columns[1]))
-            {
-                $previous_pids[] = $columns[1];
-            }
-        }
-
-        pclose(popen("start /B php-cgi -b 127.0.0.1:9123", "r"));
-
-        $current_pids = [];
-        $fd = popen("tasklist /fi \"imagename eq php-cgi.exe\" /fo CSV /nh", "r");
-        $output = stream_get_contents($fd);
-        pclose($fd);
-
-        $lines = explode("\n", $output);
-
-        foreach ($lines as $line)
-        {
-            $columns = explode("\",\"", $line);
-            
-            if (isset($columns[1]))
-            {
-                $current_pids[] = $columns[1];
-            }
-        }
-
-        $pid = \array_diff($current_pids, $previous_pids);
-
-        if (empty($pid))
-            return -2;
-
-        $pid = \array_pop($pid);
-
-        \file_put_contents($pid_file, \intval($pid));
-
-        return 0;
+        return $this->startProcess("php-cgi", "php-cgi -b 127.0.0.1:9123");
     }
 
-    private function stopWindows(ConsoleContext $ctx) : int
+    public function stop(ConsoleContext $ctx) : bool
     {
-        $pid_file = $ctx->toLocalPath("/.tmp/php/.php-cgi.pid");
-        if (!\file_exists($pid_file))
-            return -1;
-
-        $pid = \file_get_contents($pid_file);
-
-        if (empty($pid) || !\is_numeric($pid))
-            return -2;
-
-        pclose(popen("taskkill /F /pid {$pid}", "r"));
-
-        \unlink($pid_file);
-
-        return 0;
+        return $this->stopProcess("php-cgi");
     }
 }
--- src/Console/PHP/ServerCommand.php
+++ src/Console/PHP/ServerCommand.php
@@ -4,8 +4,13 @@ namespace Gekko\Console\PHP;
 
 use \Gekko\Console\{ ConsoleContext, Command};
 
-class ServerCommand extends Command
+class ServerCommand extends PHPCommand
 {
+    public function __construct(ConsoleContext $ctx)
+    {
+        parent::__construct($ctx);
+    }
+
     public function run(ConsoleContext $ctx) : int
     {
         // argv[1] should be the ServerCommand's name
@@ -17,104 +22,17 @@ class ServerCommand extends Command
 
         $operation = $ctx->getArguments()[2];
         
-        $is_windows = strcasecmp(substr(PHP_OS, 0, 3), 'WIN') == 0;
-
         if ($operation === "start")
         {
-            if ($is_windows) {
-                $this->startWindows($ctx);
-            } 
+            $pid = $this->startProcess("php-server", "php -S localhost:8080 index.php");
 
-            return 0;
+            return $pid > 0 ? 0 : -1;
         }
         else if ($operation === "stop")
         {
-            if ($is_windows) {
-                $this->stopWindows($ctx);
-            }
+            return $this->stopProcess("php-server") ? 0 : -1;
         }
 
         return -2;
-    }
-
-    private function startWindows(ConsoleContext $ctx) : int
-    {
-        $dir = $ctx->toLocalPath("/.tmp/php");
-
-        if (!\file_exists($dir))
-            mkdir($dir);
-
-        $pid_file = "{$dir}/.php-server.pid";
-
-        if (\file_exists($pid_file))
-        {
-            \fwrite(STDOUT, "PHP server is already running (PID " . \file_get_contents($pid_file) .  ")");
-            return -1;
-        }
-
-        $previous_pids = [];
-        $fd = popen("tasklist /fi \"imagename eq php.exe\" /fo CSV /nh", "r");
-        $output = stream_get_contents($fd);
-        pclose($fd);
-
-        $lines = explode("\n", $output);
-
-        foreach ($lines as $line)
-        {
-            $columns = explode("\",\"", $line);
-            
-            if (isset($columns[1]))
-            {
-                $previous_pids[] = $columns[1];
-            }
-        }
-
-        pclose(popen("start /B php -S localhost:8080 index.php", "r"));
-
-        $current_pids = [];
-        $fd = popen("tasklist /fi \"imagename eq php.exe\" /fo CSV /nh", "r");
-        $output = stream_get_contents($fd);
-        pclose($fd);
-
-        $lines = explode("\n", $output);
-
-        foreach ($lines as $line)
-        {
-            $columns = explode("\",\"", $line);
-            
-            if (isset($columns[1]))
-            {
-                $current_pids[] = $columns[1];
-            }
-        }
-
-        $pid = \array_diff($current_pids, $previous_pids);
-
-        if (empty($pid))
-            return -2;
-
-        $pid = \array_pop($pid);
-
-        \file_put_contents($pid_file, \intval($pid));
-
-        return 0;
-    }
-
-    private function stopWindows(ConsoleContext $ctx) : int
-    {
-        $pid_file = $ctx->toLocalPath("/.tmp/php/.php-server.pid");
-        if (!\file_exists($pid_file))
-            return -1;
-
-        $pid = \file_get_contents($pid_file);
-
-        if (empty($pid) || !\is_numeric($pid))
-            return -2;
-
-        pclose(popen("taskkill /F /pid {$pid}", "r"));
-
-        \unlink($pid_file);
-
-        return 0;
     }
 }
